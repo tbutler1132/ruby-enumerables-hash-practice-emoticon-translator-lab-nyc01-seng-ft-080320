1bb19ca4c59ad192a24eb4d0d83bc4c487375e4c
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-30 11:56:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file, e_emoticon)
   apology = "Sorry, that emoticon was not found"
   emoticons.each do |emotion, lang|
     lang.each do |inner_key,emoticon|
+      binding.pry 
       if emoticon == e_emoticon
         j_emoticon = emotion
       end
