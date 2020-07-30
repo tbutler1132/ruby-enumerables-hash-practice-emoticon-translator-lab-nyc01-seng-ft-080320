9351840c9cebf9e6c4f7d13e35a98f48d6dc6242
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-30 11:47:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,49 @@
-# require modules here
+require 'yaml'
+require 'pry' # require modules here
 
-def load_library
-  # code goes here
+def load_library(file_path = 'lib/emoticons.yml')
+  emoticon_library = YAML.load_file(file_path)
+  result = emoticon_library.each_with_object({}) do |(key, value), final_hash|
+    if !final_hash[key]
+      final_hash[key] = {
+        :english => value[0],
+        :japanese => value[1]
+      }
+    end
+    result
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, e_emoticon)
+  emoticons = load_library(file)
+  j_meaning = ""
+  apology = "Sorry, that emoticon was not found"
+  emoticons.each do |emotion, lang|
+    lang.each do |inner_key,emoticon|
+      if emoticon == e_emoticon
+        j_meaning = emotion
+      end
+    end
+    if j_meaning == ""
+      j_meaning = apology
+    end
+  end
+  j_meaning
 end
-
-def get_english_meaning
-  # code goes here
+#
+def get_english_meaning(file,j_emoticon)
+  emoticons = load_library(file)
+  eng_meaning = ""
+  apology = "Sorry, that emoticon was not found"
+  emoticons.each do |emotion, lang|
+    lang.each do |inner_key,emoticon|
+      if emoticon == j_emoticon
+        eng_meaning = emotion
+      end
+    end
+    if eng_meaning == ""
+      eng_meaning = apology
+    end
+  end
+  eng_meaning
 end
